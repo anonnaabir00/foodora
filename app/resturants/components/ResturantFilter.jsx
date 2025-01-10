3db36c794f24b22fd44b5446ad70e9cf8ced8f78
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-01-10 20:04:23 +0600

    Added

---
+++ app/resturants/components/ResturantFilter.jsx
@@ -0,0 +1,48 @@
+export default function ResturantFilter() {
+    return (
+                <div className="food-icons">
+                    <div className="container food-icons_inner">
+                        <div className="icon-list">
+                            <img src="./images/Group 241.png"/>
+                            <p>Burger</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (1).png"/>
+                            <p>Pizza</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (2).png"/>
+                            <p>Snack</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (3).png"/>
+                            <p>Chicken</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (4).png"/>
+                            <p>Coffee</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (5).png"/>
+                            <p>Breads</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (6).png"/>
+                            <p>Cake</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (7).png"/>
+                            <p>Dosa</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (8).png"/>
+                            <p>Omelet</p>
+                        </div>
+                        <div className="icon-list">
+                            <img src="./images/Group 241 (9).png"/>
+                            <p>Hotdog</p>
+                        </div>
+                    </div>
+                </div>
+    );
+}
